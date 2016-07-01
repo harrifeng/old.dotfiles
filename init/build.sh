5ef51f6d68a082ec63719335ac3f57b172e416c8
Harri Feng <harrifeng@gmail.com>
2016-07-01 11:21:20 +0800

    new init sript

---
+++ init/build.sh
@@ -0,0 +1,5 @@
+ln -s ~/github/dotemacsd ~/.emacs.d
+ln -s ~/github/dotfiles/docker ~/.docker
+ln -s ~/github/dotfiles/tumx/old/tmux.conf ~/.tmux.conf
+ln -s ~/github/dotfiles/bash/basic/noproxy.sh ~/.bashrc
+mkdir -p ~/virtualpy
