a3e1f0bd39d5e011ebfbab3410a07ab91b6eb2de
Haoran Feng <haoran.feng@sap.com>
2016-07-09 06:11:38 +0000

    use init.sh

--- init/build.sh
+++
@@ -1,5 +0,0 @@
-ln -s ~/github/dotemacsd ~/.emacs.d
-ln -s ~/github/dotfiles/docker ~/.docker
-ln -s ~/github/dotfiles/tumx/old/tmux.conf ~/.tmux.conf
-ln -s ~/github/dotfiles/bash/basic/noproxy.sh ~/.bashrc
-mkdir -p ~/virtualpy
