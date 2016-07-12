cc0e6905a627a8f3b7d5ac5f092140db414fe2ab
Harri Feng <harrifeng@gmail.com>
2016-07-12 02:29:09 +0000

    for pip error

--- bash/basic/noproxy.sh
+++ bash/basic/noproxy.sh
@@ -3,6 +3,7 @@ export EDITOR=emacs
 export TERM=xterm-256color
 export PATH=/usr/local/bin:$PATH
 export LANG=en_US.UTF-8
+export LC_ALL=C
 
 # git show branch------------------------------------------>
 parse_git_branch() {
